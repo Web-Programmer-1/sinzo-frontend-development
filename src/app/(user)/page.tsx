90e8a14b73fcb3bc77542d961b7562429c19fa59
tecnology-expart <mdhakimshorkar123@gmail.com>
2026-04-21 23:55:29 +0600

    added-new

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,10 +1,9 @@
 
 import HomePage from '../../components/_HomePage/HomePage'
-// import CustomerDashboard from '../../components/_USERDASHBOARD/overview/CustomarDashboardOverview'
 export default function HomePages() {
   return (
     <div>
-      {/* home page */}
+      {/* home page is comment */}
      <HomePage></HomePage>
     </div>
   )
