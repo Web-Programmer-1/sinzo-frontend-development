86bc49ea62e78ea4a32383f2e28ebfcc55a67f52
tecnology-expart <mdhakimshorkar123@gmail.com>
2026-04-21 23:49:58 +0600

    added-latest

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+
 import HomePage from '../../components/_HomePage/HomePage'
 
 export default function HomePages() {
   return (
     <div>
+      {/* home page */}
      <HomePage></HomePage>
     </div>
   )
